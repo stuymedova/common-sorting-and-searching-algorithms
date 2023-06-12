d82f89a72255fbadc9fec70fb5bbe6e93f0ac507
Sofya Tuymedova <stuymedova@gmail.com>
2023-06-12 22:13:52 +0300

    Add description

--- sorting-algorithms/quickSort.js
+++ sorting-algorithms/quickSort.js
@@ -1,13 +1,22 @@
 // Quick Sort
 //
-// - Complexity (Scalability): O(n log(n)).
+// - Complexity (Scalability): O(n log(n)) on average,
+//   O(n^2) in the worst case.
+//
+// Divide and conquer algorithm. Works by choosing the
+// pivot element and swapping the elements such that all
+// elements less than the pivot come before elements
+// greather than than the pivot. The procedure is repeated
+// until the array is sorted.
+//
+// Additional resources:
+// - https://www.youtube.com/watch?v=SLauY6PpjW4
 
 
 import swapInPlace from './utilities/swapInPlace.js';
 
 export default function quickSort(givenArray) {
-	let sortedArray = [...givenArray];
-	return _quickSort(sortedArray, 0, sortedArray.length - 1);
+	return _quickSort(givenArray, 0, givenArray.length - 1);
 }
 
 function _quickSort(givenArray, leftPointer, rightPointer) {
